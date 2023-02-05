d69c714b49798208cdb2f02f31db1a6294e4a6fd
Hedesil <mariano25294@gmail.com>
2023-02-05 22:26:32 +0100

    Object instance

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 const MEMORY_SIZE: number = 4096;
 
-interface machine_t<T> {
+interface Machine_t {
     memory: number;
     V: number[];
 }
+
+let machine_t: Machine_t = {
+    memory: MEMORY_SIZE,
+    V: []
+};
